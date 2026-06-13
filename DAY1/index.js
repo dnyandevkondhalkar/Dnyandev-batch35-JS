5992f61e3723640055f237f74302b8205e5f6d71
dnyandevkondhalkar <166099542+dnyandevkondhalkar@users.noreply.github.com>
2026-06-13 07:28:19 +0530

    JS DAY3 updates

---
+++ DAY1/index.js
@@ -0,0 +1,170 @@
+var age = 25;
+
+console.log(age, age, age);
+
+
+
+
+
+
+
+
+// var number = 1; // declare and assign
+// console.log(number);
+
+// number = 2; // re assign
+// console.log(number);
+
+// var number = 3; // re declare and assign
+// console.log(number);
+
+
+
+
+
+
+
+
+let age1 = 25;
+console.log(age1);
+
+age2 = 26;
+console.log(age2);
+
+let age3 = 27;
+console.log(age3);
+
+
+
+
+
+
+
+
+
+// const number1 = 20;
+// console.log(number1);
+
+// number2 = 21;
+// console.log(number2);
+
+// const number3 = 22;
+// console.log(number3);
+
+
+
+
+
+
+
+
+//                 var     let      const
+// reassign        yes      yes       no
+
+// redeclare       yes      no        no
+
+// hoisting        yes     no        no
+
+// scope         function  block     block
+
+
+
+
+
+
+
+age = 25;
+console.log(age);
+
+var age;
+
+
+
+
+
+
+// age = 25;
+// console.log(age);
+
+// let age;
+
+
+
+
+
+
+
+var age6 = 25;
+// let age = 25;
+// const age = 25;
+{
+  console.log(age6);
+}
+
+// {} - block
+
+
+
+
+
+
+
+// {
+//   const age7 = 25;
+//   let age7 = 25;
+// }
+
+// console.log(age7);
+
+
+
+
+
+
+
+
+
+
+var number2;
+var number1 = 1;
+{
+  number2 = 2;
+  let number3 = 3;
+  const number4 = 4;
+  console.log(number4);
+  {
+    console.log(number3);
+    console.log(number2);
+  }
+}
+
+
+// console.log(number1);
+// var let const 
+// {
+//     var let const 
+// }
+
+// {
+//     var let const 
+// }
+// var 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(number);
+number = 20;
+console.log(number);
+
+var number;
